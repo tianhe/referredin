df9575284e6179d722ccbb74d27192b5d68aa50b
Tian He <tian.he@gojee.com>
2013-05-23 00:53:18 -0400

    remove stale controller

--- app/controllers/home_controller.rb
+++
@@ -1,9 +0,0 @@
-class HomeController < ApplicationController
-
-  def index
-    @user = current_user
-    @facet = params[:facet]
-    @rankings = @user.contacts.order_by_count(@facet)
-  end
-
-end
